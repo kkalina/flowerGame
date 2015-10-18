c8c1476688f4dffb44d1cb078b706cabb123624d
kkalina <kkalina@umich.edu>
2015-10-18 03:21:34 -0400

    more sound impovement

--- Assets/bulletShoot.cs
+++ Assets/bulletShoot.cs
@@ -5,6 +5,7 @@ public class bulletShoot : MonoBehaviour
 {
 
     public GameObject bullet;
+    public GameObject bulletSoundObj;
 
     public float frequency = 1f;
     private float lastOccruance = 0f;
@@ -23,6 +24,8 @@ public class bulletShoot : MonoBehaviour
                 GameObject bulletInstance = Instantiate(bullet);
                 bulletInstance.transform.position = this.transform.position;
                 bulletInstance.transform.rotation = this.transform.rotation;
+                GameObject bulletSoundInstance = Instantiate(bulletSoundObj);
+                bulletSoundInstance.transform.position = this.transform.position;
                 //bulletInstance.GetComponent<Rigidbody>().velocity = this.transform.
                 //muzzleLight.GetComponent<Light>().enabled = true;
                 lastOccruance = Time.time;
