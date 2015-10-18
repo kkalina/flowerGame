6a3587b25d56bb32025a0ea313e3882d40ab06ca
Becarls <becarls@umich.edu>
2015-10-18 03:36:49 -0400

    Here you go ben

--- Assets/_Script/Driver.cs
+++ Assets/_Script/Driver.cs
@@ -2,11 +2,28 @@
 using System.Collections;
 
 public class Driver : MonoBehaviour {
+    public GameObject sphere;
     public AudioSource welcome;
+    public bool playAudio = true;
+    private bool pickADaisy = true;
+    private bool waterPlants = false;
     
 	// Use this for initialization
-	void Start () {
-	
+	void Update() {
+        if (pickADaisy) {
+            if (playAudio) {
+                welcome.Play();
+                playAudio = false;
+            }
+            if (sphere.GetComponent<Objective1>().grabbing) {
+                pickADaisy = false;
+                playAudio = true;
+                waterPlants = true;
+            }
+        }
+
+        //CHANGE THE THING WE CAN PICK UP
+        
 	}
 	
 	
