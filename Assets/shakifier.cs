102d5b9c9b858eeda42e836032ee3e14ec96f5e1
kkalina <kkalina@umich.edu>
2015-10-18 04:50:24 -0400

    new character prefab

---
+++ Assets/shakifier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shakifier : MonoBehaviour
+{
+    public GameObject anchor;
+    public float intensity = 0.1f;
+    public bool shaking = false;
+    
+    void Update()
+    {
+        if (shaking)
+        {
+            this.transform.position = new Vector3(anchor.transform.position.x + (Random.value * intensity), anchor.transform.position.y + (Random.value * intensity), anchor.transform.position.z + (Random.value * intensity));
+        }
+        else
+        {
+            this.transform.position = anchor.transform.position;
+        }
+    }
+}
