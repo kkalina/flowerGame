7618794306b2679d88e1f53a37bfde37020053bb
kkalina <kkalina@umich.edu>
2015-10-18 00:59:13 -0400

    stuffs :+1:

---
+++ Assets/bulletShoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletShoot : MonoBehaviour {
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
