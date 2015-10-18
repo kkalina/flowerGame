542db14b0642da77e3e0d19857c21ff81aea753c
kkalina <kkalina@umich.edu>
2015-10-18 01:43:09 -0400

    things

---
+++ Assets/bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+    public float speed = 100f;
+    public GameObject hitExplosion;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody>().velocity = this.transform.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "rex")
+        {
+            GameObject hit = Instantiate(hitExplosion);
+            hit.transform.position = this.transform.position;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- Assets/bulletShoot.cs
+++ Assets/bulletShoot.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bulletShoot : MonoBehaviour {
+public class bulletShoot : MonoBehaviour
+{
 
     public GameObject bullet;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float frequency = 1f;
+    private float lastOccruance = 0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > lastOccruance + frequency)
+            {
+                GameObject bulletInstance = Instantiate(bullet);
+                bulletInstance.transform.position = this.transform.position;
+                bulletInstance.transform.rotation = this.transform.rotation;
+                //bulletInstance.GetComponent<Rigidbody>().velocity = this.transform.
+                //muzzleLight.GetComponent<Light>().enabled = true;
+                lastOccruance = Time.time;
+            }
+        }
+    }
 }
