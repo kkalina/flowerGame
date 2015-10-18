21473f0ce4b74d5d4288c8ac522bf8803d641006
xenthe <njmckay@umich.edu>
2015-10-17 23:17:57 -0400

    Sphere of influence
    can now interact w/ da stufs

---
+++ Assets/Objective1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective1 : MonoBehaviour {
+	
+	public GameObject Sphere_of_influence;
+	bool grabable = false;
+	public GameObject plant;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag=="Plant_1")
+		{
+			grabable = true;
+			
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag=="Plant_1")
+		{
+			grabable = false;
+		}
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (grabable == true) {
+			if (Input.GetButtonDown("Submit"))
+				plant.SetActive(false);
+		}
+	}
+}
